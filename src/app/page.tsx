ad301abf7005c290e5d8426cf80f2d9e1f476d8c
TanNguyen <minhtan86a@gmail.com>
2024-10-18 11:30:49 +0700

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
-import { Button } from "@/components/ui/button";
+import qs from "qs";
+import { HeroSection } from "@/components/custom/hero-section";
+
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      on: {
+        "layout.hero-section": {
+          populate: {
+            image: {
+              fields: ["url", "alternativeText"],
+            },
+            link: {
+              populate: true,
+            },
+          },
+        },
+      },
+    },
+  },
+});
 
 async function getStrapiData(path: string) {
   const baseUrl = "http://localhost:1337";
+
+  const url = new URL(path, baseUrl);
+  console.log(url);
+  url.search = homePageQuery;
+
   try {
-    const response = await fetch(baseUrl + path);
+    const response = await fetch(url.href, { cache: "no-store" });
     const data = await response.json();
     return data;
   } catch (error) {
@@ -13,14 +38,14 @@ async function getStrapiData(path: string) {
 
 export default async function Home() {
   const strapiData = await getStrapiData("/api/home-page");
-  //console.log(strapiData);
 
-  const { title, description } = strapiData.data;
+  console.dir(strapiData, { depth: null });
+
+  const { blocks } = strapiData.data;
 
   return (
-    <main className="container mx-auto py-6">
-      <h1 className="text-5xl font-bold"> {title}</h1>
-      <p className="text-xl mt-4">{description}</p>
+    <main>
+      <HeroSection data={blocks[0]} />
     </main>
   );
 }
